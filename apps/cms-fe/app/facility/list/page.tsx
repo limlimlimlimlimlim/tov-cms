83a88cadd331304daa68a31b6e9cbffd3cdb3370
lim-lim-lim <lim831128@gmail.com>
2023-10-17 02:13:11 +0900

    feat: #22 create facility list ui

--- apps/cms-fe/app/facility/list/page.tsx
+++ apps/cms-fe/app/facility/list/page.tsx
@@ -1,3 +1,187 @@
+"use client";
+import { Button, Flex, Form, Input, Select, Table } from "antd";
+import { useCallback, useEffect, useState } from "react";
+import { format } from "date-fns";
+import type { ColumnsType } from "antd/es/table";
+import Link from "next/link";
+import { EditOutlined } from "@ant-design/icons";
+import type { FacilityItem } from "../../../interface/facility";
+import BuldingInfoManagementModal from "../../../component/building-info-management/building-info-management-modal";
+
+const { Search } = Input;
+const { Option } = Select;
+
 export default function FacilityList() {
-  return <div>FacilityList</div>;
+  const [count, setCount] = useState(17);
+  const [data, setData] = useState<FacilityItem[]>([]);
+  const [isCategoryManagementModalOpen, setIsOpenCategoryManagementModal] =
+    useState(false);
+
+  const columns: ColumnsType<FacilityItem> = [
+    {
+      title: "번호",
+      dataIndex: "no",
+      width: 80,
+    },
+    {
+      title: "층",
+      dataIndex: "floor",
+      width: 100,
+    },
+    {
+      title: "동",
+      dataIndex: "building",
+      width: 100,
+    },
+    {
+      title: "구분",
+      dataIndex: "type",
+      width: 120,
+    },
+    {
+      title: "구분상세",
+      dataIndex: "detailType",
+      width: 120,
+    },
+    {
+      title: "시설명",
+      dataIndex: "name",
+    },
+    {
+      title: "위치설정",
+      dataIndex: "position",
+      width: 100,
+    },
+    {
+      title: "미리보기",
+      width: 100,
+      render: () => <Button size="small">미리보기</Button>,
+    },
+    {
+      title: "등록일",
+      dataIndex: "createDate",
+      width: 180,
+      render: (date: Date) => format(date, "yyyy-MM-dd hh:mm:ss"),
+    },
+    {
+      title: "최종 수정일",
+      dataIndex: "modifiedDate",
+      width: 180,
+      render: (date: Date) => format(date, "yyyy-MM-dd hh:mm:ss"),
+    },
+    {
+      title: "",
+      width: 80,
+      render: (value: any) => {
+        return (
+          <Link href={`/floor-map/edit/${(value as any).no}`}>
+            <Button size="small" type="text">
+              <EditOutlined />
+            </Button>
+          </Link>
+        );
+      },
+    },
+  ];
+
+  useEffect(() => {
+    const temp: FacilityItem[] = [];
+    for (let i = 0; i < 100; i++) {
+      temp.push({
+        key: i,
+        no: i,
+        building: "중앙",
+        floor: "1",
+        name: "name",
+        type: "부대시설",
+        detailType: "키오스크",
+        position: "완료",
+        createDate: new Date(),
+        modifiedDate: new Date(),
+      });
+    }
+    setData(temp);
+  }, []);
+
+  const onSearch = useCallback(() => {
+    console.log("on search");
+  }, []);
+
+  return (
+    <>
+      <Flex vertical gap="middle">
+        <Flex justify="space-between">
+          <Flex gap="large">
+            <Form.Item label="층 선택">
+              <Select style={{ width: 100 }} defaultValue="all">
+                <Option key="all" value="all">
+                  전체
+                </Option>
+                <Option key="floor1" value="floor1">
+                  1층
+                </Option>
+                <Option key="floor2" value="floor2">
+                  2층
+                </Option>
+                <Option key="floor3" value="floor3">
+                  3층
+                </Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="동 선택">
+              <Select style={{ width: 200 }} defaultValue="all">
+                <Option key="all" value="all">
+                  전체
+                </Option>
+                <Option key="building1" value="building1">
+                  중앙
+                </Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="구분">
+              <Select style={{ width: 100 }} defaultValue="all">
+                <Option key="all" value="all">
+                  전체
+                </Option>
+                <Option key="facility" value="facility">
+                  시설
+                </Option>
+                <Option key="ancillary" value="ancillary">
+                  부대시설
+                </Option>
+              </Select>
+            </Form.Item>
+          </Flex>
+          <Button
+            onClick={() => {
+              setIsOpenCategoryManagementModal(true);
+            }}
+          >
+            카테고리 관리
+          </Button>
+        </Flex>
+
+        <Flex justify="space-between">
+          <span>Total : {count}</span>
+          <Search
+            placeholder="검색어를 입력해주세요."
+            onSearch={onSearch}
+            style={{ width: 300 }}
+          />
+        </Flex>
+        <Table
+          columns={columns}
+          dataSource={data}
+          pagination={{ pageSize: 50 }}
+          scroll={{ y: 750 }}
+        />
+      </Flex>
+      <BuldingInfoManagementModal
+        open={isCategoryManagementModalOpen}
+        onCancel={() => {
+          setIsOpenCategoryManagementModal(false);
+        }}
+      />
+    </>
+  );
 }
---
+++ apps/cms-fe/interface/facility.ts
@@ -0,0 +1,12 @@
+export interface FacilityItem {
+  key: React.Key;
+  no: number;
+  building: string;
+  floor: string;
+  name: string;
+  type: string;
+  detailType: string;
+  position: string;
+  createDate: Date;
+  modifiedDate: Date;
+}

24e78724dd94c066feae198fd27b1d1df8bb3120
lim-lim-lim <lim831128@gmail.com>
2023-10-16 21:37:09 +0900

    feat: #17 create building info modal component

--- apps/cms-fe/app/floor-map/list/page.tsx
+++ apps/cms-fe/app/floor-map/list/page.tsx
@@ -6,6 +6,7 @@ import type { ColumnsType } from "antd/es/table";
 import Link from "next/link";
 import { EditOutlined } from "@ant-design/icons";
 import type { FloorMapItem } from "../../../interface/floor-map";
+import BuldingInfoManagementModal from "../../../component/building-info-management-modal/building-info-management-modal";
 
 const { Search } = Input;
 const { confirm } = Modal;
@@ -77,6 +78,8 @@ export default function KioskList() {
   const [count, setCount] = useState(17);
   const [data, setData] = useState<FloorMapItem[]>([]);
   const [selectedData, setSelectedData] = useState<FloorMapItem[]>([]);
+  const [isBuildingManagementModalOpen, setIsBuildingManagementModalOpen] =
+    useState(false);
 
   useEffect(() => {
     const temp: FloorMapItem[] = [];
@@ -117,73 +120,92 @@ export default function KioskList() {
     },
   };
 
+  const onClickOkUpdateBuildingInfoData = useCallback(() => {
+    void message.success("건물 정보가 수정됐습니다.");
+    setIsBuildingManagementModalOpen(false);
+  }, []);
+
   return (
-    <Flex vertical gap="middle">
-      <Flex justify="space-between">
-        <Flex gap="large">
-          <Form.Item label="층 선택">
-            <Select style={{ width: 100 }} defaultValue="all">
-              <Option key="all" value="all">
-                전체
-              </Option>
-              <Option key="floor1" value="floor1">
-                1층
-              </Option>
-              <Option key="floor2" value="floor2">
-                2층
-              </Option>
-              <Option key="floor3" value="floor3">
-                3층
-              </Option>
-            </Select>
-          </Form.Item>
-          <Form.Item label="동 선택">
-            <Select style={{ width: 200 }} defaultValue="all">
-              <Option key="all" value="all">
-                전체
-              </Option>
-              <Option key="building1" value="building1">
-                중앙
-              </Option>
-            </Select>
-          </Form.Item>
-        </Flex>
-        <Button>건물 정보 관리</Button>
-      </Flex>
-
-      <Flex justify="space-between">
-        <Flex gap="small" align="center">
+    <>
+      <Flex vertical gap="middle">
+        <Flex justify="space-between">
+          <Flex gap="large">
+            <Form.Item label="층 선택">
+              <Select style={{ width: 100 }} defaultValue="all">
+                <Option key="all" value="all">
+                  전체
+                </Option>
+                <Option key="floor1" value="floor1">
+                  1층
+                </Option>
+                <Option key="floor2" value="floor2">
+                  2층
+                </Option>
+                <Option key="floor3" value="floor3">
+                  3층
+                </Option>
+              </Select>
+            </Form.Item>
+            <Form.Item label="동 선택">
+              <Select style={{ width: 200 }} defaultValue="all">
+                <Option key="all" value="all">
+                  전체
+                </Option>
+                <Option key="building1" value="building1">
+                  중앙
+                </Option>
+              </Select>
+            </Form.Item>
+          </Flex>
           <Button
-            danger
-            disabled={selectedData.length === 0}
-            onClick={onClickDelete}
+            onClick={() => {
+              setIsBuildingManagementModalOpen(true);
+            }}
           >
-            삭제
+            건물 정보 관리
           </Button>
-          <Link href="/kiosk/register">
-            <Button type="primary">수정</Button>
-          </Link>
+        </Flex>
 
-          <span>Total : {count}</span>
+        <Flex justify="space-between">
+          <Flex gap="small" align="center">
+            <Button
+              danger
+              disabled={selectedData.length === 0}
+              onClick={onClickDelete}
+            >
+              삭제
+            </Button>
+            <Link href="/kiosk/register">
+              <Button type="primary">수정</Button>
+            </Link>
+
+            <span>Total : {count}</span>
+          </Flex>
+          <Flex>
+            <Search
+              placeholder="검색어를 입력해주세요."
+              onSearch={onSearch}
+              style={{ width: 300 }}
+            />
+          </Flex>
         </Flex>
-        <Flex>
-          <Search
-            placeholder="검색어를 입력해주세요."
-            onSearch={onSearch}
-            style={{ width: 300 }}
-          />
-        </Flex>
+        <Table
+          columns={columns}
+          dataSource={data}
+          pagination={{ pageSize: 50 }}
+          scroll={{ y: 750 }}
+          rowSelection={{
+            type: "checkbox",
+            ...rowSelection,
+          }}
+        />
       </Flex>
-      <Table
-        columns={columns}
-        dataSource={data}
-        pagination={{ pageSize: 50 }}
-        scroll={{ y: 750 }}
-        rowSelection={{
-          type: "checkbox",
-          ...rowSelection,
+      <BuldingInfoManagementModal
+        open={isBuildingManagementModalOpen}
+        onCancel={() => {
+          setIsBuildingManagementModalOpen(false);
         }}
       />
-    </Flex>
+    </>
   );
 }
---
+++ apps/cms-fe/component/building-info-management-modal/building-info-management-modal.tsx
@@ -0,0 +1,38 @@
+import { Modal, Tabs } from "antd";
+import { useCallback } from "react";
+
+interface ComponentProps {
+  open: boolean;
+  onCancel: () => void;
+}
+
+const TabPane = Tabs.TabPane;
+
+export default function BuldingInfoManagementModal({
+  open,
+  onCancel,
+}: ComponentProps) {
+  const updateBuildingInfoData = useCallback(() => {}, []);
+
+  return (
+    <Modal
+      title="건물 정보 관리"
+      open={open}
+      okText="수정"
+      cancelText="취소"
+      onOk={updateBuildingInfoData}
+      onCancel={() => {
+        onCancel();
+      }}
+    >
+      <Tabs type="card">
+        <TabPane tab="한글" key="ko">
+          Content of Tab Pane 1
+        </TabPane>
+        <TabPane tab="영어" key="en">
+          Content of Tab Pane 2
+        </TabPane>
+      </Tabs>
+    </Modal>
+  );
+}

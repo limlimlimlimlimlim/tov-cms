5dec573a123867b9c8b024d9e0feb0f09cb98770
lim-lim-lim <lim831128@gmail.com>
2023-10-17 12:43:37 +0900

    feat: #23 create category list ui

--- apps/cms-fe/component/category-management/category-list.tsx
+++ apps/cms-fe/component/category-management/category-list.tsx
@@ -1,14 +1,50 @@
-import { Flex } from "antd";
-import { useCallback } from "react";
+import { Flex, Input, Modal, message } from "antd";
+import { useCallback, useState } from "react";
+// eslint-disable-next-line import/no-cycle
 import CategoryItem from "./category-item";
+
+const { confirm } = Modal;
 
 interface ComponentProps {
   categories: any[];
 }
 
 export default function CategoryList({ categories }: ComponentProps) {
-  const onAdd = useCallback(() => {}, []);
-  const onRemove = useCallback(() => {}, []);
+  const [newCategoryName, setNewCategoryName] = useState("");
+  const onAdd = useCallback(() => {
+    confirm({
+      title: "카테고리 추가",
+      okText: "확인",
+      cancelText: "취소",
+      content: (
+        <Flex vertical>
+          <span>카테고리 이름을 입력해주세요.</span>
+          <Input
+            onChange={(e) => {
+              setNewCategoryName(e.target.value);
+            }}
+          />
+        </Flex>
+      ),
+      onOk() {
+        console.log(newCategoryName);
+        void message.success("카테고리가 생성됐습니다.");
+      },
+    });
+    confirm;
+  }, [newCategoryName]);
+
+  const onRemove = useCallback(() => {
+    confirm({
+      title: "카테고리 삭제",
+      okText: "확인",
+      cancelText: "취소",
+      content: "카테고리를 삭제하시겠습니까?",
+      onOk() {
+        void message.success("카테고리가 삭제됐습니다.");
+      },
+    });
+  }, []);
   const onChange = useCallback(() => {}, []);
 
   const createItems = useCallback(() => {

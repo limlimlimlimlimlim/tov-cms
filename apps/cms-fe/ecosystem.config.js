c738c2b1001de6c4c957b2d4404cef9145bcb262
lim-lim-lim <lim831128@gmail.com>
2023-12-21 01:00:16 +0900

    클러스터 조정

--- apps/cms-fe/ecosystem.config.js
+++ apps/cms-fe/ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: 'npm',
       args: 'start',
       exec_mode: 'cluster',
-      instances: 2,
+      instances: 1,
       autorestart: true,
       listen_timeout: 50000,
       kill_timeout: 5000,

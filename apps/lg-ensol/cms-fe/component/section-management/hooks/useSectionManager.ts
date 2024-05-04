9babae026f7ed64cc537b7cf872b50f7bb030426
limlimlimlimlimlim <limlimlim.work@gmail.com>
2024-05-05 03:22:54 +0900

    feat: #126 패닝

--- apps/lg-ensol/cms-fe/component/section-management/hooks/useSectionManager.ts
+++ apps/lg-ensol/cms-fe/component/section-management/hooks/useSectionManager.ts
@@ -1,11 +1,44 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 declare const window;
 
 const useSectionManager = () => {
   const [canvas, setCanvas] = useState<any>();
+  const isDragging = useRef(false);
+  const lastDragPosition = useRef({ x: 0, y: 0 });
+
   const createCanvas = (canvas, options?) => {
-    setCanvas(new window.fabric.Canvas(canvas, options));
+    const c = new window.fabric.Canvas(canvas, options);
+    setCanvas(c);
+    initCanvasMouseEvent(c);
+  };
+
+  const initCanvasMouseEvent = (canvas) => {
+    canvas.on('mouse:down', ({ e }) => {
+      if (e.altKey) {
+        isDragging.current = true;
+        lastDragPosition.current = { x: e.clientX, y: e.clientY };
+        canvas.selection = false;
+      }
+    });
+
+    canvas.on('mouse:move', ({ e }) => {
+      if (isDragging.current) {
+        const vpt = canvas.viewportTransform;
+        vpt[4] += e.clientX - lastDragPosition.current.x;
+        vpt[5] += e.clientY - lastDragPosition.current.y;
+        canvas.requestRenderAll();
+        lastDragPosition.current = { x: e.clientX, y: e.clientY };
+      }
+    });
+
+    canvas.on('mouse:up', () => {
+      if (isDragging.current) {
+        canvas.setViewportTransform(canvas.viewportTransform);
+        canvas.selection = true;
+        isDragging.current = false;
+      }
+    });
   };
 
   const setMapImage = (url) => {
--- apps/lg-ensol/cms-fe/component/section-management/section-management.tsx
+++ apps/lg-ensol/cms-fe/component/section-management/section-management.tsx
@@ -1,8 +1,8 @@
 import Script from 'next/script';
 import { useEffect, useRef } from 'react';
+import { Button, Flex } from 'antd';
 import { getBaseUrl } from '../../util/axios-client';
 import useSectionManager from './hooks/useSectionManager';
-import { Button, Flex } from 'antd';
 
 declare const window;
 

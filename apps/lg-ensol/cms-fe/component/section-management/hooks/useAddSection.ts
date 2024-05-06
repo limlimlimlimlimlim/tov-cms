dfc8ec5053dcc0bf9584048de2c3dffe33c59090
limlimlimlimlimlim <limlimlim.work@gmail.com>
2024-05-06 21:44:44 +0900

    feat: #127 구역 영역 지정 리팩토링

--- apps/lg-ensol/cms-fe/component/section-management/hooks/useAddSection.ts
+++ apps/lg-ensol/cms-fe/component/section-management/hooks/useAddSection.ts
@@ -5,10 +5,23 @@ declare const fabric: any;
 const useAddSection = () => {
   const canvas = useRef<any>();
   const currentPolygonPoints = useRef<any[]>([]);
+  const currentPolygonCircles = useRef<any[]>([]);
   const currentPolygonLines = useRef<any[]>([]);
+  const guideLine = useRef<any>();
 
   const init = (c) => {
     canvas.current = c;
+    const guide = new fabric.Line([], {
+      stroke: 'blue',
+      strokeWidth: 2,
+      selectable: false,
+    });
+    guide.set('x1', 0);
+    guide.set('y1', 0);
+    guide.set('x2', 0);
+    guide.set('y2', 0);
+    guideLine.current = guide;
+
     initEvents();
   };
 
@@ -16,6 +29,7 @@ const useAddSection = () => {
     if (!canvas.current) return;
     canvas.current.on('mouse:down', onMouseDown);
     canvas.current.on('mouse:move', onMouseMove);
+    // canvas.current.on('mouse:up', onMouseMove);
   };
 
   const addPolygon = () => {
@@ -32,83 +46,118 @@ const useAddSection = () => {
     return currentPolygonPoints.current[0];
   };
 
+  const getLastPoint = () => {
+    return currentPolygonPoints.current[
+      currentPolygonPoints.current.length - 1
+    ];
+  };
+
   const getLastLine = () => {
     return currentPolygonLines.current[currentPolygonLines.current.length - 1];
   };
 
   const onMouseDown = (e) => {
     if (!canvas.current) return;
+    if (e.e.altKey) return;
     if (e.target === currentPolygonPoints.current[0]) {
       addPolygon();
     }
     if (e.target) return;
     canvas.current.selection = false;
-    const lastLine: any = getLastLine();
-    const x = lastLine ? lastLine.x2 : e.absolutePointer.x;
-    const y = lastLine ? lastLine.y2 : e.absolutePointer.y;
+    const { x, y } = e.absolutePointer;
+    currentPolygonPoints.current.push({ x, y });
+    canvas.current.add(guideLine.current);
+    guideLine.current.set('x1', x);
+    guideLine.current.set('y1', y);
+    guideLine.current.set('x2', x);
+    guideLine.current.set('y2', y);
+    forceUpdatePolygon();
+  };
 
-    const circle = new fabric.Circle({
-      radius: 10,
-      fill: 'yellow',
-      stroke: 'green',
-      strokeWidth: 3,
-      left: x,
-      top: y,
-      originX: 'center',
-      originY: 'center',
-      selectable: false,
+  const forceUpdatePolygon = () => {
+    removePolygon();
+    createPolygon();
+    canvas.current.renderAll();
+  };
+
+  const createPolygon = () => {
+    currentPolygonPoints.current.forEach(({ x, y }, index: number) => {
+      if (index > 0) {
+        const prevPoint = currentPolygonPoints.current[index - 1];
+        const line = new fabric.Line([], {
+          stroke: 'blue',
+          strokeWidth: 2,
+          selectable: false,
+        });
+        line.set('x1', prevPoint.x);
+        line.set('y1', prevPoint.y);
+        line.set('x2', x);
+        line.set('y2', y);
+        canvas.current.add(line);
+        currentPolygonLines.current.push(line);
+      }
     });
 
-    circle.on('mousedblclick', () => {
-      canvas.current.remove(circle);
-      canvas.current.renderAll();
+    currentPolygonPoints.current.forEach(({ x, y }, index) => {
+      const circle = new fabric.Circle({
+        radius: 10,
+        fill: 'yellow',
+        stroke: 'green',
+        strokeWidth: 3,
+        left: x,
+        top: y,
+        originX: 'center',
+        originY: 'center',
+        selectable: false,
+      });
+      canvas.current.add(circle);
+      currentPolygonCircles.current.push(circle);
+
+      circle.on('mousedblclick', () => {
+        currentPolygonPoints.current.splice(index, 1);
+        forceUpdatePolygon();
+      });
     });
+  };
 
-    currentPolygonPoints.current.push(circle);
-    canvas.current.add(circle);
-
-    const line = new fabric.Line([], {
-      stroke: 'blue',
-      strokeWidth: 2,
-      selectable: false,
+  const removePolygon = () => {
+    currentPolygonCircles.current.forEach((c) => {
+      canvas.current.remove(c);
     });
-    line.set('x1', x);
-    line.set('y1', y);
-    line.set('x2', x);
-    line.set('y2', y);
-    currentPolygonLines.current.push(line);
-    canvas.current.add(line);
-
-    canvas.current.on('mouse:up', onMouseMove);
+    currentPolygonLines.current.forEach((l) => {
+      canvas.current.remove(l);
+    });
+    currentPolygonCircles.current = [];
+    currentPolygonLines.current = [];
   };
 
   const onMouseMove = (e) => {
-    const lastLine = getLastLine();
+    const lastPoint = getLastPoint();
 
-    if (!lastLine) return;
+    if (!lastPoint) return;
     if (!e.absolutePointer) return;
     if (e.e.shiftKey) {
-      const { x1, y1 } = lastLine;
+      const { x, y } = lastPoint;
       const radian =
         Math.round(
-          Math.atan2(e.absolutePointer.y - y1, e.absolutePointer.x - x1) * 10,
+          Math.atan2(e.absolutePointer.y - y, e.absolutePointer.x - x) * 10,
         ) / 10;
 
       const degree = (radian * 180) / Math.PI;
       const roundDgreeToNearest10 = roundToNearest(degree);
       const roundRadianToNearest10 = (roundDgreeToNearest10 * Math.PI) / 180;
       const radius = Math.sqrt(
-        Math.pow(e.absolutePointer.y - y1, 2) +
-          Math.pow(e.absolutePointer.x - x1, 2),
+        Math.pow(e.absolutePointer.y - y, 2) +
+          Math.pow(e.absolutePointer.x - x, 2),
       );
 
-      const x2 = x1 + radius * Math.cos(roundRadianToNearest10);
-      const y2 = y1 + radius * Math.sin(roundRadianToNearest10);
-      lastLine.set('x2', x2);
-      lastLine.set('y2', y2);
+      const x2 = x + radius * Math.cos(roundRadianToNearest10);
+      const y2 = y + radius * Math.sin(roundRadianToNearest10);
+      guideLine.current.set('x2', x2);
+      guideLine.current.set('y2', y2);
     } else {
-      lastLine.set('x2', e.absolutePointer.x);
-      lastLine.set('y2', e.absolutePointer.y);
+      guideLine.current.set('x2', e.absolutePointer.x);
+      guideLine.current.set('y2', e.absolutePointer.y);
     }
     canvas.current.requestRenderAll();
   };
